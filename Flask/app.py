d37eae567492a1dabe54a77bf5e5e18e98ee2fb2
reasley <33018600+reasley-com@users.noreply.github.com>
2021-05-13 01:26:03 +0900

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -3,12 +3,17 @@
 from flask import Flask
 from flask import request
 from flask import send_file
+from flask import url_for
+from flask import redirect
+from flask import flash
 from flask import make_response
 from flask import render_template
 from werkzeug.utils import secure_filename
 
 # Pymongo
 import pymongo
+import gridfs
+
 
 # Encrypt
 import base64
@@ -22,12 +27,17 @@ from bson.binary import Binary
 #################### Init ####################
 # Flask Settings
 app = Flask(__name__)
+app.secret_key = 'reasley'
 
 
 # Pymongo Settings
-connection = pymongo.MongoClient()
-db = connection.db_name    # Database Name
-file_meta = db.collection_name  # Database Collection Name
+connection = pymongo.MongoClient()  # Mongo DB Client Connection
+db = connection.db_name             # Database Name
+
+normal_meta = db.collection_name    # Database Collection Name
+grid_meta   = db.fs.files           # Database Collection Name
+
+fs          = gridfs.GridFS(db)     # GRIDFS Settings
 
 # No Cache
 @app.after_request
@@ -41,7 +51,9 @@ def set_response_headers(response):
 #################### Flask ####################
 @app.route('/')
 def index():
-    return render_template('index.html')
+    normal_list = normal_meta.find()
+    grid_list   = grid_meta.find()
+    return render_template('index.html', normal_list=normal_list, grid_list=grid_list)
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -55,23 +67,57 @@ def upload():
             encoded = Binary(f.read())
 
         # DB Insert File data
-        file_meta.insert({"filename": file.filename, "file": encoded})
-        return 'success'
+        normal_meta.insert({"filename": file.filename, "file": encoded})
+        
+        flash('success')
+        return redirect(url_for('index'))
     except:
-        return 'fail'
-    
+        flash('fail')
+        return redirect(url_for('index'))
 
 @app.route('/download/<string:fileId>')
 def download(fileId):
     # Find Database Field
     query = {'_id': ObjectId(fileId)}
-    cursor = file_meta.find(query)
+    cursor = normal_meta.find(query)
 
     # Get File
     fileName = cursor[0]['filename']
 
     # Download File
     return send_file(fileName, attachment_filename=fileName, as_attachment=True)
+
+
+
+@app.route('/gridfs_upload', methods=['POST'])
+def gridfs_upload():
+    try:
+        # Get File Data
+        file = request.files['file']
+
+        # Save File
+        file.save(secure_filename(file.filename))
+        with open(file.filename, "rb") as f:
+            encoded = Binary(f.read())
+
+        # DB Insert File data
+        res = fs.put(encoded, filename=file.filename)
+        flash('success')
+        return redirect(url_for('index'))
+    except:
+        flash('fail')
+        return redirect(url_for('index'))
+
+    
+
+@app.route('/gridfs_download/<string:fileId>')
+def gridfs_download(fileId):
+    # Find File
+    filedId = ObjectId(fileId)
+    file = fs.get(filedId)
+
+    # Download File
+    return send_file(file, attachment_filename=file.filename, as_attachment=True)
 
 
 app.run(debug=True, host="0.0.0.0", port=80)
